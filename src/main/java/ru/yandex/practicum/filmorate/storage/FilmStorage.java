09a5faac6efe11648a5e09342fbb827ec902ea4b
Anastasia Konts <nastyaaa022@yandex.ru>
2026-06-20 00:33:18 +0700

    fix: исправить стиль кода/2

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -6,9 +6,14 @@ import java.util.List;
 import java.util.Optional;
 
 public interface FilmStorage {
+
     Film save(Film film);
+
     void delete(long id);
+
     Optional<Film> findById(long id);
+
     List<Film> getAll();
+
     Film update(Film film);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -6,12 +6,18 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserStorage {
+
     User save(User user);
+
     void delete(long id);
+
     Optional<User> findById(long id);
+
     List<User> getAll();
+
     User update(User user);
 
     Optional<User> findByEmail(String email);
+
     Optional<User> findByLogin(String login);
 }
